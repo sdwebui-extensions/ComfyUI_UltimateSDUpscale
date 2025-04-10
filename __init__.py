5b4cd84fe2077865b25964e8932aa46b6cbc2be8
宣源 <xuanyuan.lb@alibaba-inc.com>
2025-04-10 15:01:39 +0800

    fix import

--- __init__.py
+++ __init__.py
@@ -5,34 +5,34 @@ import os
 current_dir = os.path.dirname(os.path.realpath(__file__))
 repos_dir = os.path.join(current_dir, "repositories")
 usdu_dir = os.path.join(repos_dir, "ultimate_sd_upscale")
-if not len(os.listdir(usdu_dir)):
-    print("[USDU] Original USDU script not found, downloading it from https://github.com/Coyote-A/ultimate-upscale-for-automatic1111")
-    import urllib.request
-    import zipfile
-    import shutil
+# if not len(os.listdir(usdu_dir)):
+#     print("[USDU] Original USDU script not found, downloading it from https://github.com/Coyote-A/ultimate-upscale-for-automatic1111")
+#     import urllib.request
+#     import zipfile
+#     import shutil
 
-    url = "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111/archive/master.zip"
-    zip_path = os.path.join(current_dir, "usdu_temp.zip")
+#     url = "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111/archive/master.zip"
+#     zip_path = os.path.join(current_dir, "usdu_temp.zip")
 
-    urllib.request.urlretrieve(url, zip_path)
+#     urllib.request.urlretrieve(url, zip_path)
 
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        top_folder = zip_ref.namelist()[0].split('/')[0] + '/'
-        for member in zip_ref.namelist():
-            if member.startswith(top_folder) and not member.endswith('/'):
-                target_path = os.path.join(usdu_dir, member[len(top_folder):])
-                os.makedirs(os.path.dirname(target := os.path.join(usdu_dir, member[len(top_folder):])), exist_ok=True)
-                with zip_ref.open(member) as source, open(target, 'wb') as target_file:
-                    shutil.copyfileobj(fsrc=zip_ref.open(member), fdst=target_file)
+#     with zipfile.ZipFile(zip_path, "r") as zip_ref:
+#         top_folder = zip_ref.namelist()[0].split('/')[0] + '/'
+#         for member in zip_ref.namelist():
+#             if member.startswith(top_folder) and not member.endswith('/'):
+#                 target_path = os.path.join(usdu_dir, member[len(top_folder):])
+#                 os.makedirs(os.path.dirname(target := os.path.join(usdu_dir, member[len(top_folder):])), exist_ok=True)
+#                 with zip_ref.open(member) as source, open(target, 'wb') as target_file:
+#                     shutil.copyfileobj(fsrc=zip_ref.open(member), fdst=target_file)
 
-    os.remove(zip_path)
-    print("[USDU] Original USDU script downloaded successfully")
+#     os.remove(zip_path)
+#     print("[USDU] Original USDU script downloaded successfully")
 
 # Remove other custom_node paths from sys.path to avoid conflicts
-custom_node_paths = [path for path in sys.path if "custom_node" in path]
-original_sys_path = sys.path.copy()
-for path in custom_node_paths:
-    sys.path.remove(path)
+# custom_node_paths = [path for path in sys.path if "custom_node" in path]
+# original_sys_path = sys.path.copy()
+# for path in custom_node_paths:
+#     sys.path.remove(path)
 
 # Add this repository's path to sys.path for third-party imports
 repo_dir = os.path.dirname(os.path.realpath(__file__))
@@ -40,20 +40,20 @@ sys.path.insert(0, repo_dir)
 original_modules = sys.modules.copy()
 
 # Place aside potentially conflicting modules
-modules_used = [
-    "UltimateSDUpsacle_modules",
-    "UltimateSDUpsacle_modules.devices",
-    "UltimateSDUpsacle_modules.images",
-    "UltimateSDUpsacle_modules.processing",
-    "UltimateSDUpsacle_modules.scripts",
-    "UltimateSDUpsacle_modules.shared",
-    "UltimateSDUpsacle_modules.upscaler",
-    "UltimateSDUpsacle_utils",
-]
-original_imported_modules = {}
-for module in modules_used:
-    if module in sys.modules:
-        original_imported_modules[module] = sys.modules.pop(module)
+# modules_used = [
+#     "UltimateSDUpsacle_modules",
+#     "UltimateSDUpsacle_modules.devices",
+#     "UltimateSDUpsacle_modules.images",
+#     "UltimateSDUpsacle_modules.processing",
+#     "UltimateSDUpsacle_modules.scripts",
+#     "UltimateSDUpsacle_modules.shared",
+#     "UltimateSDUpsacle_modules.upscaler",
+#     "UltimateSDUpsacle_utils",
+# ]
+# original_imported_modules = {}
+# for module in modules_used:
+#     if module in sys.modules:
+#         original_imported_modules[module] = sys.modules.pop(module)
 
 # Proceed with node setup
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
@@ -61,15 +61,15 @@ __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
 
 # Clean up imports
 # Remove any new modules
-modules_to_remove = []
-for module in sys.modules:
-    if module not in original_modules:
-        modules_to_remove.append(module)
-for module in modules_to_remove:
-    del sys.modules[module]
+# modules_to_remove = []
+# for module in sys.modules:
+#     if module not in original_modules:
+#         modules_to_remove.append(module)
+# for module in modules_to_remove:
+#     del sys.modules[module]
 
 # Restore original modules
-sys.modules.update(original_imported_modules)
+# sys.modules.update(original_imported_modules)
 
 # Restore original sys.path
-sys.path = original_sys_path
+# sys.path = original_sys_path
